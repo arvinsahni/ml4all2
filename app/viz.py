af9a77ac7ddeb61b6068627b5e2f6f93f9376413
arvinsahni <arvinsahni@arvins-MacBook-Air.local>
2017-08-17 00:55:34 +0800

    52

--- app/viz.py
+++ app/viz.py
@@ -159,7 +159,7 @@ def check_shiny():
 	while response_code != 200:
 		try:
 			#r = requests.head("https://ml4all1.herokuapp.com:"+str(port1))
-            r=requests.head("http://0.0.0.0:7775")
+			r=requests.head("http://0.0.0.0:7775")
 			response_code = r.status_code
 			print(r.status_code)
 		except requests.ConnectionError:
