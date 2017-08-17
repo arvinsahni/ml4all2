715adc893a12b38c3eb091c57e0f4684e5bba92f
arvinsahni <arvinsahni@arvins-MacBook-Air.local>
2017-08-18 04:46:46 +0800

    61

--- run.R
+++ run.R
@@ -6,8 +6,13 @@ port2=options(shiny.port = 7775)
 cat("hello cat")
 cat(port2)
 cat("done")
+#shiny::runApp(
+  #appDir = getwd(),
+  #host = '0.0.0.0',
+  #port = as.numeric(port2)
+#)
 shiny::runApp(
-  appDir = getwd(),
+  appDir = 'app/',
   host = '0.0.0.0',
   port = as.numeric(port2)
 )
