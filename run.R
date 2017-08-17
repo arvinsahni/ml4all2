2d6a4a2592170d25b0a2e02e34a06555f9565b78
arvinsahni <arvinsahni@arvins-MacBook-Air.local>
2017-08-18 06:17:27 +0800

    66

--- run.R
+++ run.R
@@ -1,18 +1,18 @@
 library(shiny)
 
 #port <- Sys.getenv('PORT')
-port2=options(shiny.port = 7775)
+#port2=options(shiny.port = 7775)
 
-cat("hello cat")
-cat(port2)
-cat("done")
-#shiny::runApp(
-  #appDir = getwd(),
-  #host = '0.0.0.0',
-  #port = as.numeric(port2)
-#)
-#shiny::runApp(
-  #appDir = 'app/',
-  #host = '0.0.0.0',
-  #port = as.numeric(port2)
-#)
+#cat("hello cat")
+#cat(port2)
+#cat("done")
+##shiny::runApp(
+  ##appDir = getwd(),
+  ##host = '0.0.0.0',
+  ##port = as.numeric(port2)
+##)
+##shiny::runApp(
+  ##appDir = 'app/',
+  ##host = '0.0.0.0',
+  ##port = as.numeric(port2)
+##)
